e0b90fd8d52fb4cb2c6859a4e7a35a77953be421
Lindsay Johnston <lindsaykjohnston@outlook.com>
2020-07-24 15:22:24 -0700

    display none for message on mobile when map comes

--- docs/app.js
+++ docs/app.js
@@ -486,7 +486,7 @@ async function getMap(cityArray) {
     if(isMobile){
         document.getElementById('map-div').style.width='90%';
         document.getElementById('map-div').style.height= '50vh';
-        document.getElementById('message').innerHTML = '';
+        document.getElementById('message').style.display='none';
     } 
 
     map = new google.maps.Map(
