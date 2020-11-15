56b364e31667f585a7e6b8bd58c98608c82a0ca6
Lindsay Johnston <lindsaykjohnston@outlook.com>
2020-11-14 21:38:14 -0600

    Heroku test

--- docs/app.js
+++ docs/app.js
@@ -515,6 +515,7 @@ function createMarker(latLng, cityName, numberOfUsers) {
 
 function markerClickHandler(event) {
     console.log(event);
+    console.log("test Heroku");
     let clickedMarker;
 
     if (event.ub.target.attributes.src === undefined) {
